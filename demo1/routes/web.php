f9ece616fe0dfd759bd3dbd88b65500ac4a153ef
dat <dathalongbay@gmail.com>
2021-07-16 18:57:58 +0700

    111

--- demo1/routes/web.php
+++ demo1/routes/web.php
@@ -21,3 +21,27 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+
+use Illuminate\Support\Facades\DB;
+// https://github.com/barryvdh/laravel-debugbar
+// https://laravel.com/docs/8.x/queries
+Route::get('/db1', function () {
+
+    // lấy ra data trong bảng users
+    $users = DB::table('users')->get();
+
+    foreach ($users as $user) {
+        echo "<p>$user->email</p>";
+    }
+    dump($users);
+    // chuyển collection sang mảng
+    $users2 = $users->toArray();
+
+    dump($users2);
+});
+
+Route::get("/db2", function () {
+    // ->first() lấy ra 1 bản ghi đầu tiên và duy nhất
+    $user = DB::table('users')->where('email', 'user@itsolutionstuff.com')->where('is_admin', 0)->first();
+    dump($user);
+});
